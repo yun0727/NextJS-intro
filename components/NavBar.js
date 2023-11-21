4c2452a67260ca588d1027fe1c52d37bf56e66ca
yun <austru00@gmail.com>
2023-11-21 16:29:46 +0900

    2.1 Fetching Data

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,29 +1,49 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
-import styles from "./NavBar.module.css"
 
 export default function NavBar(){
   const router = useRouter();
   return(
     <nav>
-      <Link legacyBehavior href="/" >
-          <a className={router.pathname === "/" ? "active" : ""}>
-            Home
+      <img src="/vercel.svg" />
+      <div>
+        <Link legacyBehavior href="/" >
+            <a className={router.pathname === "/" ? "active" : ""}>
+              Home
+            </a>
+        </Link>
+        <Link legacyBehavior href="/about">
+          <a className={router.pathname === "/about" ? "active": ""}>
+            About
           </a>
-          
-      </Link>
-      <Link legacyBehavior href="/about">
-        <a className={router.pathname === "/about" ? "active": ""}>
-          About
-        </a>
-      </Link>
+        </Link>
+      </div>
       <style jsx>{`
-      a{
-        text-decoration:none;
+      nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
       }
       .active{
         color: tomato;
       }
+      nav div {
+          display: flex;
+          gap: 10px;
+        }
       `}</style>
     </nav>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "62d0519bf70d5097a329be057a7f727d";
+
 export default function Home(){
+  const [movies, setMovies] = useState();
+  useEffect(()=>{
+    (async()=>{
+      const {results} = await(
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  },[])
   return (
     <div>
       <Seo title="Home"/>
-      <h1 className="active">Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie)=>(
+        <div key={movie.id} >
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   )
 }
