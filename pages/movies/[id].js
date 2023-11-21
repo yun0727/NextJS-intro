c5e669ed0edf084a103b6a3607a98f81e34cbfea
yun <austru00@gmail.com>
2023-11-21 17:49:08 +0900

    2.6 Movie Detail

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -2,7 +2,10 @@ import { useRouter } from "next/router";
 
 export default function Detail(){
   const router = useRouter();
-  console.log(router);
-  return "detail"
+  return (
+    <div>
+      <h4>{router.query.title || "Loading..."}</h4>
+    </div>
+  )
 
 }
